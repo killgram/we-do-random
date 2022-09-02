eace7ff6e7bf2b3b22c1b64efd6364c550464709
killgram <killgram94@gmail.com>
2022-09-02 13:31:21 +0600

    add typescript configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,52 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020, // enable parsing latest ECMAScript
+    sourceType: 'module', // allow use of imports
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+  rules: {
+    semi: 'off',
+    indent: 'off',
+    'max-len': ['off', { ignoreTemplateLiterals: true, ignoreStrings: true }],
+    'quote-props': 'off',
+    'operator-linebreak': 'off',
+    'object-curly-spacing': 'off',
+    'prettier/prettier': 'off',
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: false,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: true,
+          FunctionExpression: true,
+        },
+      },
+    ],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          String: false,
+          Boolean: false,
+          Number: false,
+          Symbol: false,
+          '{}': false,
+          Object: false,
+          object: false,
+          Function: false,
+        },
+        extendDefaults: true,
+      },
+    ],
+  },
+}
---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import type { Node } from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'
+
+const App: () => Node = () => {
+  return (
+    <View>
+      <Text>sdfsdf</Text>
+      <Text>sdfsdf</Text>
+    </View>
+  )
+}
+
+export default App
